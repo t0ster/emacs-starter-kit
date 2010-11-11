ef877a542ccc9caf46dd5acde263dadcb005a2a2
Roman Dolgiy <roman@bravetoasterstudio.com>
2010-11-11 21:30:06 +0200

    Adding custom key bindings

---
+++ t0ster/keys.el
@@ -0,0 +1,8 @@
+;; Split Windows
+(global-set-key [f6] 'split-window-horizontally)
+(global-set-key [f7] 'split-window-vertically)
+;; (global-set-key [f8] 'delete-window)
+
+;; Some Mac-friendly key counterparts
+(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-z") 'undo)
