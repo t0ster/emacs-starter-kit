1f25b0b7e939c182992af4d485864bf2cf8389e6
Roman Dolgiy <roman@bravetstudio.com>
2011-03-21 19:19:19 +0200

    Highlighting 79 column in python-mode

--- t0ster/t0ster-python.el
+++ t0ster/t0ster-python.el
@@ -1,2 +1,4 @@
 ;; Newline and indent on RET instead of C-j
 ;; (add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
+(require 'column-marker)
+(add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 79)))
