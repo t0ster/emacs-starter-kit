2ca6c76413499d7747c4648da31fa4da2a1695a3
Roman Dolgiy <roman@bravetstudio.com>
2011-09-21 01:10:14 +0300

    Added sr-speedbar

--- t0ster/t0ster-theme.el
+++ t0ster/t0ster-theme.el
@@ -5,3 +5,8 @@
 (blink-cursor-mode)
 (global-hl-line-mode 1)
 (set-face-attribute 'default nil :height 140)
+
+(require 'sr-speedbar)
+(setq sr-speedbar-right-side nil)
+(setq speedbar-use-images nil)
+(setq sr-speedbar-width-x 36)
