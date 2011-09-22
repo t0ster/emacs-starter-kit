9cc573e861ec0755625aff871ca86dfb7fa603e5
Roman Dolgiy <tosters@gmail.com>
2011-09-22 23:18:16 +0300

    Temporary disabled speedbar

--- t0ster/t0ster-theme.el
+++ t0ster/t0ster-theme.el
@@ -6,10 +6,10 @@
 (global-hl-line-mode 1)
 (set-face-attribute 'default nil :height 140)
 
-(require 'sr-speedbar)
-(setq sr-speedbar-right-side nil)
-(setq speedbar-use-images nil)
-(sr-speedbar-toggle)
+;; (require 'sr-speedbar)
+;; (setq sr-speedbar-right-side nil)
+;; (setq speedbar-use-images nil)
+;; (sr-speedbar-toggle)
 
 (setq ac-auto-start nil)
 (setq ac-quick-help-delay 0.5)
